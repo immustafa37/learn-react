3800607d8d471975ccf8c1724f1e885f5faeec0b
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-02 19:46:42 -0700

    Importing and Exporting Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
+import Login from "./UserComponents";
+
 function App() {
   return (
     <div>
-      <Header />
+      <Login />
       <h1>Main Content</h1>
     </div>
   )
 }
 
-function Header(){
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  )
-}
 export default App;
