29bc2143b18aff816fcb34102bf2c36a1b9b5314
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-03 20:14:56 -0700

    JSX exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import pic from './assets/pic.jpg';
+
 function App() {
-  
+  return (
+    <div>
+      <h1>Mustafa</h1>
+      <img src={pic} alt="Pic" />
+      <ul>
+        <li>pic1</li>
+        <li>pic2</li>
+      </ul>
+      <button onClick={() => {
+        alert("called")
+      }}>click</button>
+    </div>
+  );
 }
 
 export default App;
