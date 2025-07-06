a4e2b44052cee19c50cded03e7adf1a3043e1c21
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-05 19:37:32 -0700

    JavaScript in JSX with Curly Braces-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import pic from './assets/pic.jpg';
-
 function App() {
+  const name = "Mustafa";
   return (
     <div>
-      <h1>Mustafa</h1>
-      <img src={pic} alt="Pic" />
-      <ul>
-        <li>pic1</li>
-        <li>pic2</li>
-      </ul>
-      <button onClick={() => {
-        alert("called")
-      }}>click</button>
+      <h1>{name}</h1>
     </div>
   );
 }
