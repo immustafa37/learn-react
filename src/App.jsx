d0e2af3954487a61861fb4b9694600d83978c2e0
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-05 20:18:42 -0700

    JavaScript if else condition in JSX with Curly Braces-5

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ function App() {
   function sum(a, b){
     return a+b
   }
+  function opration(a, b, op){
+    if(op=="+"){
+      return a+b
+    }else if(op=="-"){
+      return a-b
+    }else{
+      return a*b
+    }
+  }
   return (
     <div>
       <h1>{name}</h1>
       <h1>{x + y}</h1>
       <h1>{demo()}</h1>
       <h1>{sum(10, 40)}</h1>
+      <h1>{opration(10, 20, "+")}</h1>
     </div>
   );
 }
