45a27cac08bdb28e37a78dae6b8319551ffad5f8
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-07 19:35:19 -0700

    State in react JS-4

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,12 +2,17 @@ import { useState } from "react"
 
 const Counter = ()=>{
     const [count, setCount] = useState(0);
+    const [reverseCount, setReverse]= useState(10);
     return (
         <div>
             <h1>Count:{count}</h1>
             <button onClick={()=>{
                 setCount(count + 1)
             }}>Update Count</button>
+            <h1>Reverse Count:{reverseCount}</h1>
+            <button onClick={()=>{
+                setReverse(reverseCount - 1)
+            }}>Reverse Counte</button>
         </div>
     )
 }
