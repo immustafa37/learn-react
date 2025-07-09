e66e12852be1ec204bc634bed601d1a27585632b
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-08 21:00:54 -0700

    update. And now learn Next js

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from "react";
-import Counter from "./components/Counter";
-
 function App() {
-  const [name, setName] = useState("Mustafa");
-  const handleName = ()=> {
-    setName("Mustafa Ahsan")
-  }
   return (
     <div>
-      <h1>{name}</h1>
-      <button onClick={handleName}>Change Name</button>
-      <Counter />
     </div>
   );
 }
