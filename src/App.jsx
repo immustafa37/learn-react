facf56d352fecb6acf833b283c3c2f3a9c8ceec0
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-14 20:09:31 -0700

    Multiple Condition or Else if(Ternary oprator)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 function App() {
-  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Hide and Show</h1>
-      <button onClick={()=> {
-        setDisplay(!display)
-      }}>Hide & Show</button>
+      <h1>Multiple Condition or Else if</h1>
+      <h2>{count}</h2>
+
+      <button onClick={()=>{
+        setCount(count + 1)
+      }} >Counter</button>
       {
-        display? <h1>Mustafa</h1>: null
+        count==0? <h1>Condition 0</h1>
+        :count==1? <h1>Condition 1</h1>
+        :count==2? <h1>Condition 2</h1>
+        :count==3? <h1>Condition 3</h1>
+        :count==4? <h1>Condition 4</h1>
+        :count==5? <h1>Condition 5</h1>
+        : <h1>Other Condition</h1>
       }
 
     </div>
