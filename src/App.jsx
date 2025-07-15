0b837f820a51a8845835debc406471476a5ab5b3
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-14 19:54:46 -0700

    toggle in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 function App() {
+  const [display, setDisplay] = useState(false);
   return (
     <div>
+      <h1>Toggle in React</h1>
+      {
+        display? <h1>Mustafa</h1>: <h1>no user</h1>
+      }
+
     </div>
   );
 }
